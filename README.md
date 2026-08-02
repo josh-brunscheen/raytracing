7d5188f86cee56f048538445598f3c03aeb2c5cf
Joshua Brunscheen <114787256+josh-brunscheen@users.noreply.github.com>
2026-08-01 21:20:11 -0400

    Code cleanup

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # raytracing
-First attempt at a raytracing project in C++.
+First attempt at a raytracing project in C++ with 3D graphing additions that I made.
 
 I used the commands below for compiling, running, and then converting the produced image to a png.
 
-First run this:
+First run this to compile everything:
 
 ```console
-c++ main.cpp vec3.h color.h ray.h hittable.h hittable_list.h sphere.h camera.h interval.h rtweekend.h material.h -o rt.exe
+c++ main.cpp vec3.h color.h ray.h hittable.h hittable_list.h sphere.h camera.h interval.h rtweekend.h material.h grapher.h -o rt.exe
 ```
 
-Then this:
+Direct the output to a .ppm image file (we will convert this later):
 ```console
 ./rt.exe > image.ppm
 ```
 
-Then, finally, this:
+Then, finally, you can convert the output image into something like a .png:
 ```console
 convert image.ppm tmp.png
 ```
 
-Now, there should be file called "tmp.png" in the root folder. (pngs are usually more accesible to view)
+Now, there should be file called "tmp.png" in the root folder. 
 
 
 Sources:
--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "material.h"
-#include "plane.h"
 #include "grapher.h"
 
 #include <cmath>
@@ -24,54 +23,7 @@ int main() {
   Grapher testFunction(sineWave, 2, 2, -3, 3, -3, 3, sphereMaterial);
   world.add(testFunction.getPoints());
 
-  // SINE FUNCTION Below
-  //auto color_material = make_shared<lambertian>(color(0.203125, 0.7421875, 0.92578125));
-  // for (int x = -5; x < 5; x++) {
-  //   for (int y = -4; y < 4; y++) {
-  //     // auto color_material = make_shared<lambertian>(color(std::abs(x/7), std::abs(y/4), std::abs((x + y) / 15)));
-  //     auto color_material = make_shared<metal>(color((x + 10) / double(20), (x + 10) / double(20), 0.9), 0.0);
-      // world.add(make_shared<sphere>(point3(x, y, std::sin(x) + std::sin(y)), 0.5, color_material));
-  //   }
-  // }
-  // SINE FUNCTION END
-
-  // for (int a = -11; a < 11; a++) {
-  //   for (int b = -11; b < 11; b++) {
-  //     auto choose_mat = random_double();
-  //     point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-
-  //     if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-  //       shared_ptr<material> sphere_material;
-
-  //       if (choose_mat < 0.8) {
-  //         //diffuse
-  //         auto albedo = color::random() * color::random();
-  //         sphere_material = make_shared<lambertian>(albedo);
-  //         world.add(make_shared<sphere> (center, 0.2, sphere_material));
-  //       } else if (choose_mat < 0.95) {
-  //         //metal
-  //         auto albedo = color::random(0.5, 1);
-  //         auto fuzz = random_double(0, 0.5);
-  //         sphere_material = make_shared<metal>(albedo, fuzz);
-  //         world.add(make_shared<sphere>(center, 0.2, sphere_material));
-  //       } else {
-  //         //glass
-  //         sphere_material = make_shared<dielectric>(1.5);
-  //         world.add(make_shared<sphere>(center, 0.2, sphere_material));
-  //       }
-  //     }
-  //   }
-  // }
-
-  // auto material1 = make_shared<dielectric>(1.5);
-  // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-  // auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-  // world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-  // auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-  // world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
+  // vvv Camera settings (mess around them to experiment) vvv
   camera cam;
 
   cam.aspect_ratio = 16.0 / 9.0;
@@ -87,50 +39,7 @@ int main() {
   cam.defocus_angle = 0.6;
   cam.focus_dist = 22.3; //Usually 10.0
 
-  //^^ final scene ^^
 
-  // auto R = std::cos(pi/4);
-
-  // auto material_left = make_shared<lambertian>(color(0, 0, 1));
-  // auto material_right = make_shared<lambertian>(color(1, 0, 0));
-
-  // world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-  // world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
-
-  // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  // auto material_left = make_shared<dielectric>(1.50);
-  // auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-  // auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
-
-  // world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-  // world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
-  // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-  // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
-  // world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
-  // auto material_bg = make_shared<metal>(color(1.0, 1.0, 1.0), 0.0);
-  // auto material_fuzzy = make_shared<metal>(color(0.5, 0.6, 1.0), 0.5);
-  // auto material_rand = make_shared<lambertian>(color(0.1, 0.2, 0.9));
-  // auto material_met = make_shared<metal>(color(0.2, 0.6, 0.3), 0.1);
-
-  // world.add(make_shared<sphere>(point3(0.0, 0.0, -105.0), 50.0, material_bg));
-  // world.add(make_shared<sphere>(point3(-1.0, 0.0, -2.0), 0.5, material_fuzzy));
-  // world.add(make_shared<sphere>(point3(1.0, 0.0, -2.0), 0.5, material_rand));
-  // world.add(make_shared<sphere>(point3(0.0, 1.0, -2.0), 0.5, material_met));
-  
-  // camera cam;
-
-  // cam.aspect_ratio = 16.0 / 9.0;
-  // cam.image_width = 400;
-  // cam.samples_per_pixel = 100;
-  // cam.max_depth = 50;
-  // cam.vfov = 20;
-  // cam.lookfrom = point3(-2, 2, 1);
-  // cam.lookat = point3(0, 0, -1);
-  // cam.vup = vec3(0, 1, 0);
-  // cam.defocus_angle = 10.0;
-  // cam.focus_dist = 3.4;
-
+  // Create the scene :)
   cam.render(world);
 }
